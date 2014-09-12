707ce76ab582ab7e836fb2794dd6e175818c293f
tmhora <tmhora@udel.edu>
2014-09-12 11:49:58 -0400

    Update new_script.py
    Soup recipe with less salt

--- new_script.py
+++ new_script.py
@@ -3,3 +3,14 @@
 # this is the start to our beautiful collaborative project
 
 # this is the template to our pipeline
+print "10 cups of water
+ 2 carrots, sliced
+ 1 diced potato
+3 tablespoons of salt
+0.5 teaspoons of salt
+ 1 tablespoon of parsley
+ 2 tablespoons of rosemary
+1 tablespoon italian herbs
+a sprinkle of Royco usavi mix
+1 kg of minced meat"
+EOF

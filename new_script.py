5f7ffbd7671d949956fc3fd3c6aa409ba17ff2ff
tmhora <tmhora@udel.edu>
2014-09-12 11:44:24 -0400

    Update new_script.py

--- new_script.py
+++ new_script.py
@@ -3,3 +3,20 @@
 # this is the start to our beautiful collaborative project
 
 # this is the template to our pipeline
+
+# soup_recipe
+print "Grandma's Soup Recipe
+
+10 cups of water
+2 carrots, sliced
+1 diced potato
+0.5 teaspoons of salt
+1 tablespoon of parsley
+2 tablespoons of rosemary
+1 tablespoon italian herbs
+a sprinkle of Royco usavi mix
+1 kg of minced meat
+
+Boil for 8 hours, serve"
+
+EOF
